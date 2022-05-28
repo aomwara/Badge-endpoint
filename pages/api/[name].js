5ed7f4f726792fff5ec5199e0b3fcd2cb89dd86d
aom wara <waranat.cpe@gmail.com>
2022-05-29 04:33:19 +0700

    fix: api fetch

--- pages/api/[name].js
+++ pages/api/[name].js
@@ -5,14 +5,14 @@ export default function handler(req, res) {
   const { name } = req.query;
   const _app = App.find((app) => app.name === name);
 
-  fetch("https://badge-endpoint.vercel.app/images/truffle.svg").then((r) => {
-    r.text().then((d) => {
+  fetch(`https://badge-endpoint.vercel.app/images/${name}.svg`).then((r) => {
+    r.text().then((svg) => {
       res.status(200).json({
         schemaVersion: 1,
         label: " ",
         message: _app.name,
         color: _app.color,
-        logoSvg: d,
+        logoSvg: svg,
       });
     });
   });
